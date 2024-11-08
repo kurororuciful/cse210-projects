452dd39989d577ceaeaa5565df02c7fdd696af4a
kurororuciful <omarmanantan27@gmail.com>
2024-11-08 07:52:59 -0600

    Assignment attempt

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        string input = Console.ReadLine();
+        int magicNumber = int.Parse(input);
+
+        
+            Console.WriteLine("What is your guess? ");
+            string x = Console.ReadLine();
+            int guess = int.Parse(x);
+
+            /*Random randomGenerator = new Random();
+            int number = randomGenerator.Next();*/
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        
     }
 }
