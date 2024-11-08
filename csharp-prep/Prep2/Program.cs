6906ba8ac1779310f37388969eb095694757c5b0
kurororuciful <omarmanantan27@gmail.com>
2024-11-08 03:27:57 -0600

    Grade letter

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter = "";
+
+        // CORE REQUIREMENTS
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "E";
+        }
+        Console.WriteLine($"Your grade is {letter}!");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations! You PASSED! ");
+        }
+        else
+        {
+            Console.WriteLine($"Keep it up! You can do it next time. ");
+        }
     }
 }
