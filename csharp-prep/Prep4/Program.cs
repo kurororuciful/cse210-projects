70d7df2f71dcc2a1f90b6554e3e3e8ad49be1141
kurororuciful <omarmanantan27@gmail.com>
2024-11-09 19:22:50 -0600

    List Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        string ask = "Enter a number: ";
+        int x = -1;
+        int sum = 0;
+        int lar = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        while (x != 0)
+        {
+            Console.Write(ask);
+            string num = Console.ReadLine();
+            x = int.Parse(num);
+            numbers.Add(x);
+        }
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > lar)
+            {
+                lar = number;
+            }
+            /* SHORTCUT FOR ASSIGNMENT 3
+            Console.WriteLine($"The largest number is {numbers.Max()}"); */
+        }
+
+        Console.WriteLine($"The sum is: {sum} ");
+
+        float ave = ((float) sum) / (numbers.Count - 1);
+        Console.WriteLine($"The average is {ave} ");
+        Console.WriteLine($"The largest number is {lar} ");
     }
 }
