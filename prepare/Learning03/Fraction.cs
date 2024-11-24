647592900e63eec5d82048fb9b03a16cff09c5fe
kurororuciful <omarmanantan27@gmail.com>
2024-11-24 09:39:58 -0600

    created week3 assignment, still getting error

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,37 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string FractionInfo()
+    {
+        Console.WriteLine($"{_top} / {_bottom} ");
+        string = Console.ReadLine();
+    }
+
+
+    public double DecimalInfo()
+    {
+        Console.WriteLine($"{_top} / {_bottom} ")
+        double = Console.ReadLine();
+    }
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.FractionInfo());
+        Console.WriteLine(fraction1.DecimalInfo());
+        // fraction1._top    = 1;
+        // fraction1._bottom = 1;
+
+
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.FractionInfo());
+        Console.WriteLine(fraction2.DecimalInfo());
+        
+        // fraction2._top    = 6;
+        // fraction2._bottom = 1;
+
+
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.FractionInfo());
+        Console.WriteLine(fraction3.DecimalInfo());
+        
+        // fraction3._top    = 6;
+        // fraction3._bottom = 7;
+
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.FractionInfo());
+        Console.WriteLine(fraction4.DecimalInfo());
+
+
+        foreach (Fraction fraction in fraction)
+            Fraction myFraction = new Fraction();
+            {
+                myFraction.FractionInfo();
+            }
     }
 }
